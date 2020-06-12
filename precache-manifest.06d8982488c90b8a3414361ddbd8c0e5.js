b5780b4bd63c23457a33a25acec00f574d75aec9
Jessiecodes <Jessak1202@gmail.com>
2020-06-12 08:50:05 -0400

    Updates

---
+++ precache-manifest.06d8982488c90b8a3414361ddbd8c0e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b4ce91cdea21a530523e282a727511",
+    "url": "/reactsimpleportfolio/index.html"
+  },
+  {
+    "revision": "c0c74061c393462a57d9",
+    "url": "/reactsimpleportfolio/static/css/main.d815b757.chunk.css"
+  },
+  {
+    "revision": "b5dc0a9d1bc09e613dd1",
+    "url": "/reactsimpleportfolio/static/js/2.95eeacda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactsimpleportfolio/static/js/2.95eeacda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0c74061c393462a57d9",
+    "url": "/reactsimpleportfolio/static/js/main.d6e51cf2.chunk.js"
+  },
+  {
+    "revision": "3f3bb9b39e8835edb967",
+    "url": "/reactsimpleportfolio/static/js/runtime-main.a8299446.js"
+  },
+  {
+    "revision": "03a57a35423d4fe72fa5bd1354fe9ab4",
+    "url": "/reactsimpleportfolio/static/media/keyboard.03a57a35.jpg"
+  }
+]);
